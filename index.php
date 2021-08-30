f3f421d0289c03b8d1b30ec7f596c4b882506f02
cryswerton <cryswertons@gmail.com>
2021-08-29 23:39:02 -0300

    download of files were forbidden. Now it's fixed.

--- index.php
+++ index.php
@@ -2,29 +2,40 @@
 <html>
 <head>
     <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <title>Video Downloader</title>
 </head>
-<body>
-    
-    <?php
+<body class="container">
 
-        function makeLink($fileName){
-            echo "<p><a href=\"./videos2/{$fileName}\" download>{$fileName}</a></p>";
-        }
+    <header class="my-5">
 
-        if ($handle = opendir('./videos2')) {
+        <h1>PHP Video Downloader</h1>
 
-            while (false !== ($entry = readdir($handle))) {
-        
-                if ($entry != "." && $entry != ".." && $entry != ".htaccess" && $entry != "info.txt") {
-        
-                    makeLink($entry);
-                }
+    </header>
+
+    <div class="content">
+
+        <?php
+
+            function makeLink($fileName){
+                echo "<p><a href=\"./videos2/{$fileName}\" download>{$fileName}</a></p>";
             }
-        
-            closedir($handle);
-        }
-    ?>
+
+            if ($handle = opendir('./videos2')) {
+
+                while (false !== ($entry = readdir($handle))) {
+            
+                    if ($entry != "." && $entry != ".." && $entry != ".htaccess" && $entry != "info.txt") {
+            
+                        makeLink($entry);
+                    }
+                }
+            
+                closedir($handle);
+            }
+        ?>
+
+    </div>
 
 </body>
 </html>
